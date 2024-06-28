a6d3a980b09febc4cb04cc958c55d8a5971c1e1d
robbertuittenbroek <robbert.uittenbroek@rijksoverheid.nl>
2024-06-28 12:40:44 +0200

    Add more demo objects to the demo suite

--- tad/core/db.py
+++ tad/core/db.py
@@ -3,7 +3,7 @@ from functools import lru_cache
 
 from sqlalchemy.engine import Engine
 from sqlalchemy.pool import QueuePool, StaticPool
-from sqlmodel import Session, SQLModel, create_engine, select
+from sqlmodel import Session, SQLModel, create_engine, delete, select
 
 from tad.core.config import get_settings
 from tad.models import Status, Task, User
@@ -43,21 +43,50 @@ def init_db():
 
     with Session(get_engine()) as session:
         if get_settings().ENVIRONMENT == "demo":
+            if get_settings().TRUNCATE_TABLES:
+                truncate_tables(session)
+
             logger.info("Creating demo data")
-
-            user = session.exec(select(User).where(User.name == "Robbert")).first()
-            if not user:
-                user = User(name="Robbert", avatar=None)
-                session.add(user)
-
-            status = session.exec(select(Status).where(Status.name == "Todo")).first()
-            if not status:
-                status = Status(name="Todo", sort_order=1)
-                session.add(status)
-
-            task = session.exec(select(Task).where(Task.title == "First task")).first()
-            if not task:
-                task = Task(title="First task", description="This is the first task", sort_order=1, status_id=status.id)
-                session.add(task)
+            add_demo_users(session)
+            add_demo_statuses(session)
+            todo_status = session.exec(select(Status).where(Status.name == "todo")).first()
+            if todo_status is not None:
+                add_demo_tasks(session, todo_status)
             session.commit()
     logger.info("Finished initializing database")
+
+
+def truncate_tables(session: Session) -> None:
+    logger.info("Truncating tables")
+    session.exec(delete(Task))  # type: ignore
+    session.exec(delete(User))  # type: ignore
+    session.exec(delete(Status))  # type: ignore
+
+
+def add_demo_users(session: Session) -> None:
+    user = session.exec(select(User).where(User.name == "Robbert")).first()
+    if not user:
+        user = User(name="Robbert", avatar=None)
+        session.add(user)
+
+
+def add_demo_tasks(session: Session, status: Status) -> None:
+    for index in range(1, 4):
+        task = session.exec(select(Task).where(Task.title == "Example task " + str(index))).first()
+        if not task:
+            task = Task(
+                title="Example task " + str(index),
+                description="Example description " + str(index),
+                sort_order=index,
+                status_id=status.id,
+            )
+            session.add(task)
+
+
+def add_demo_statuses(session: Session) -> None:
+    for index, status_name in enumerate(["todo", "review", "in_progress", "done"]):
+        status = session.exec(select(Status).where(Status.name == status_name)).first()
+        if not status:
+            status = Status(name=status_name, sort_order=index + 1)
+            session.add(status)
+            session.commit()
